0a21c51800ad229f0ec7c14fd5a90c1376932a9b
Rajat <Rajat@DESKTOP-3J22PDM>
2020-12-02 15:22:16 +0530

    by aditya

--- GIT_project/src/com/gitDemo/Demo.java
+++ GIT_project/src/com/gitDemo/Demo.java
@@ -1,12 +1,8 @@
 package com.gitDemo;
 
 public class Demo {
-	public void sum() {
-		System.out.println("sum");
-	}
-	public void sub() {
-		System.out.println("sub");
-	}
+
+
 	public static void main(String[] args) {
 		System.out.println("main method");
 	}
