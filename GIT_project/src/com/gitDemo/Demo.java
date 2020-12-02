0a0037b88777d6c7a0e4fc81a81bf2c7cb167da0
Rajat <Rajat@DESKTOP-3J22PDM>
2020-12-02 13:51:37 +0530

    commit by aditya11

--- GIT_project/src/com/gitDemo/Demo.java
+++ GIT_project/src/com/gitDemo/Demo.java
@@ -4,6 +4,9 @@ public class Demo {
 	public void sum() {
 		System.out.println("sum");
 	}
+	public void sub() {
+		System.out.println("sub");
+	}
 	public static void main(String[] args) {
 		System.out.println("main method");
 	}
